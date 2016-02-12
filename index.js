170975a7c46e25349260790e32f6c42b25a90c34
Pablo Lagioia <pablo.lagioia@gmail.com>
2016-02-12 16:02:51 -0300

    can now send data to templates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function Engine(path) {
 }
 
 Engine.prototype.getTemplatePath = function (templateName) {
-  return this.path + "/" +  templateName.replace(/\./g, '/') + '.html';
+  return this.path + "/" +  templateName + '.html';
 };
 
 //TODO: Promisify this
@@ -28,19 +28,42 @@ Engine.prototype.replaceTemplate = function (html) {
   
 };
 
-Engine.prototype.readTemplate = function (templateName) {
+Engine.prototype.replaceData = function (html, data) {
+  
+  return html.replace(matchers.data, function (match, group) {
+    
+    var path = group.split('.'),
+        current = data;
+        
+    try {
+      
+      path.forEach(function (it) {
+        current = current[it];
+      });
+    
+    } catch (e) {
+      return "";
+    }
+    
+    return current;
+    
+  });
+  
+};
+
+Engine.prototype.readTemplate = function (templateName, data) {
   
   var self = this;
   
   return new Promise(function (resolve, reject) {
     
-    fs.readFile(self.getTemplatePath(templateName), 'utf8', function (err, data) {
+    fs.readFile(self.getTemplatePath(templateName), 'utf8', function (err, html) {
 
       if (err) {
         return reject(err);
       }
       
-      return resolve(self.replaceTemplate(data));
+      return resolve(self.replaceData(self.replaceTemplate(html), data));
       
     });
     
@@ -48,8 +71,8 @@ Engine.prototype.readTemplate = function (templateName) {
   
 };
 
-Engine.prototype.render = function(templateName) {
-  return this.readTemplate(templateName);
+Engine.prototype.render = function(templateName, data) {
+  return this.readTemplate(templateName, data);
 };
 
 module.exports = Engine;
--- matchers.js
+++ matchers.js
@@ -1,4 +1,5 @@
 module.exports = {
   'layout': /\<layout\s*src=[\"\']([A-Za-z0-9]+)[\"\']\s*\>([\n*\w\s\n*\<\>\.\'\"\=\+\-\_\,\/\?]*)<\/layout\>/m,
-  'template': /\<\s*template\s+src=[\"\']([A-Za-z0-9]+)[\"\']\s*\/\s*\>/g
+  'template': /\<\s*template\s+src=[\"\']([A-Za-z0-9]+)[\"\']\s*\/\s*\>/g,
+  'data': /\{\{([A-Za-z0-9\.]+)\}\}/g
 }
