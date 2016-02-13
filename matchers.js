a30df73a5acd9df3a782e160303220dd5862e8a6
Pablo Lagioia <pablo.lagioia@gmail.com>
2016-02-13 20:23:03 -0300

    updated matcher

--- matchers.js
+++ matchers.js
@@ -1,5 +1,5 @@
 module.exports = {
   'layout': /\<layout\s*src=[\"\']([A-Za-z0-9]+)[\"\']\s*\>([\n*\w\s\n*\<\>\.\'\"\=\+\-\_\,\/\?]*)<\/layout\>/m,
   'template': /\<\s*template\s+src=[\"\']([A-Za-z0-9]+)[\"\']\s*\/\s*\>/g,
-  'data': /\{\{([A-Za-z0-9\.]+)\}\}/g
+  'data': /\{\{([A-Za-z0-9\.\_\$]+)\}\}/g
 }
